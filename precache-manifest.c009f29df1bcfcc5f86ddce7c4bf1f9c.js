cb5f999e8789a7d4eb9568400e3f44b674466a8a
Nagendra Kumar <nagendrakumar0296@gmail.com>
2020-05-18 13:21:37 +0530

    Updates

---
+++ precache-manifest.c009f29df1bcfcc5f86ddce7c4bf1f9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13f360b3d363dc97a3d9b02986203a23",
+    "url": "/.github.io/InfraTab/index.html"
+  },
+  {
+    "revision": "21af020a9c4a07051e20",
+    "url": "/.github.io/InfraTab/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "caf3836bc8c8b43c364b",
+    "url": "/.github.io/InfraTab/static/js/2.7e437bea.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/.github.io/InfraTab/static/js/2.7e437bea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21af020a9c4a07051e20",
+    "url": "/.github.io/InfraTab/static/js/main.3b5cb56e.chunk.js"
+  },
+  {
+    "revision": "92f2f05977b551ccc18e",
+    "url": "/.github.io/InfraTab/static/js/runtime-main.be3ca0eb.js"
+  }
+]);
